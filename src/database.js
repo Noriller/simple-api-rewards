2967c559ea21120dae8450be37c5e7eadf6d61b8
Bruno <brunonoriller@gmail.com>
2022-04-03 12:02:20 -0300

    adding database tests

--- src/database.js
+++ src/database.js
@@ -3,21 +3,33 @@
  */
 function Database() {
   const data = new Map();
-  data.set('hello', 'world');
+  data.set('hello', { data: 'world' });
 
-  function get() {
-    return Array.from(data.values());
-  }
+  function get(key) {
+    if (key) {
+      return data.get(key);
+    }
 
-  function getOne(key) {
-    return data.get(key);
+    return Object.fromEntries(data);
   }
 
   function save(key, value) {
+    if (typeof value !== 'object' || value === null) {
+      throw new Error('Cannot save primitive values');
+    }
+
     data.set(key, value);
   }
 
   function update(key, value) {
+    if (typeof value !== 'object' || value === null) {
+      throw new Error('Cannot save primitive values');
+    }
+
+    if (!data.has(key)) {
+      throw new Error('Key not found');
+    }
+
     const newValue = { ...data.get(key), ...value };
     data.set(key, newValue);
   }
@@ -28,7 +40,6 @@ function Database() {
 
   return {
     get,
-    getOne,
     save,
     update,
     remove,
---
+++ src/database.spec.js
@@ -0,0 +1,80 @@
+const Database = require('./database');
+
+describe('Database', () => {
+  let database;
+
+  beforeEach(() => {
+    database = Database();
+    populateData();
+  });
+
+  it('should get the data', () => {
+    expect(database.get()).toEqual(
+      { "baz": { "foo": "qux" }, "foo": { "baz": "bar" }, "hello": { "data": "world" }, "hello2": { "data": "world2" } }
+    );
+  });
+
+  it('should get one entry', () => {
+    expect(database.get('hello2')).toEqual({ "data": "world2" });
+  });
+
+  it('should return nothing if no entry is found', () => {
+    expect(database.get('invalid')).toEqual();
+  });
+
+  it('should save data', () => {
+    const newData = { newData: 'newData' };
+    database.save('test', newData);
+    expect(database.get('test')).toEqual(newData);
+  });
+
+  it('should throw if saving primitive values', () => {
+    expect(() => database.save('test', 'test')).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', 1)).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', false)).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', BigInt(1))).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', undefined)).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', null)).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', Symbol('test'))).toThrowError('Cannot save primitive values');
+    expect(() => database.save('test', {})).not.toThrow();
+    expect(() => database.save('test', [])).not.toThrow();
+  });
+
+  it('should update data', () => {
+    const newData = { data: 'notWorld', newData: 'newData' };
+    database.update('hello', newData);
+    expect(database.get('hello')).toEqual(newData);
+  });
+
+  it('should throw if key is not found', () => {
+    expect(() => database.update('invalid', {})).toThrowError('Key not found');
+    expect(() => database.update('hello', { data: 'notWorld' })).not.toThrow();
+  });
+
+
+  it('should throw if updating with primitive values', () => {
+    expect(() => database.update('hello', 'test')).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', 1)).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', false)).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', BigInt(1))).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', undefined)).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', null)).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', Symbol('test'))).toThrowError('Cannot save primitive values');
+    expect(() => database.update('hello', {})).not.toThrow();
+    expect(() => database.update('hello', [])).not.toThrow();
+  });
+
+  it('should remove data', () => {
+    database.remove('hello');
+    expect(database.get('hello')).toEqual();
+    expect(database.get()).toEqual(
+      { "baz": { "foo": "qux" }, "foo": { "baz": "bar" }, "hello2": { "data": "world2" } }
+    );
+  });
+
+  function populateData() {
+    database.save('hello2', { data: 'world2' });
+    database.save('foo', { baz: 'bar' });
+    database.save('baz', { foo: 'qux' });
+  }
+});
